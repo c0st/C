f5cbe72cd3c6496ec7406b3f9468737bbf8350f4
Srajan Rai <thesrajanrai@gmail.com>
2022-09-11 15:34:18 +0530

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// decimal hex and octal
+
+void dec2hex(int hex);
+void dec2octal(int octal);
+
+void hex2dec(int dec);
+void hex2octal(int octal);
+
+void octal2dec(int dec);
+void octal2hex(int hex);
